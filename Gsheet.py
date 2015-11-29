f5345de4dc681fa02b85f998fb244d391c49257a
Roland Plett <roland@rnplett.org>
2015-11-28 21:04:56 -0700

    added tick data and changed the update to gsheets.

--- Gsheet.py
+++ Gsheet.py
@@ -43,13 +43,23 @@ class Downloader(object):
             #accountUpdate(msg)
         if msg.typeName == 'historicalData':
             if msg.close > 0:
-                row = msg.reqId-100
-                print row,msg.close
+                row = msg.reqId-100-5
+                p[row].value = msg.close
+                print row, msg.close, p[row].value
                 #HL1.update_cell(row,24,msg.close)
+        if msg.typeName == 'tickOptionComputation':
+            row = msg.tickerId-100-5
+            p[row].value = msg.optPrice
+            d[row].value = msg.delta
+            print row, p[row].value, d[row].value
+        if msg.typeName == 'tickPrice':
+            row = msg.tickerId-100-5
+            p[row].value = msg.price
+            print row, p[row].value, d[row].value
 
-    def requestData(self,contract):
-        self.tws.reqMktData(self._reqId, contract, '', 1)
-        self._reqId+=1
+    def requestData(self,contract,id):
+        self.tws.reqMktData(id, contract, '', 1)
+        #self._reqId+=1
 
     def requestHistoricalData(self,contract,id):
         now = datetime.datetime.now().strftime('%Y%m%d %H:%M:%S')
@@ -85,13 +95,17 @@ def accountUpdate(msg):
             Dashboard.update_acell('b41', msg.value)
             Dashboard.update_acell('c41', now)
 
-def make_contract(symbol, sec_type="STK", exch="SMART", prim_exch="NYSE", curr="USD"):
+def make_contract(symbol, sec_type="STK", exch="SMART", curr="USD", expiry="", strike="", right=""):
 
     Contract.m_symbol = symbol
     Contract.m_secType = sec_type
     Contract.m_exchange = exch
-    Contract.m_primaryExch = prim_exch
     Contract.m_currency = curr
+    if sec_type == 'OPT':
+        Contract.m_expiry = expiry
+        Contract.m_strike = strike
+        Contract.m_right = right
+
     return Contract
 
 if __name__=='__main__':
@@ -101,48 +115,56 @@ if __name__=='__main__':
     #
 
     keyFile = 'C:\Users\Roland\Google Drive\PyExchange-985ba7b9d627.json'
+    workbook = '2015b Investment workbook'
+
+    logWks = 'HL1 Log'
+    symbolCol = 2
+    expiryCol = 4
+    strikeCol = 5
+    rightCol = 6
+    priceCol = 25
+    deltaCol = 30
+    headerRow = 5
 
     #
     # Main program segment
     #
 
     gc = gsheet()
-    global Dashboard
-    global HL1
-    Dashboard = gc.wks("2015b Investment workbook","Dashboard")
-    HL1 = gc.wks("2015b Investment workbook","HL1 Log")
+
+    #  Dashboard Update
+    #
+    Dashboard = gc.wks(workbook,"Dashboard")
 
     val = Dashboard.acell('b38')
     print val
 
-    val = HL1.acell("n5")
-    print(val)
-
     dlR = Downloader(7496)
     dlN = Downloader(7497)
 
-    dlR.requestPositions()
-    dlR.requestAccounts()
-    dlN.requestPositions()
-    dlN.requestAccounts()
+    #dlR.requestPositions()
+    #dlR.requestAccounts()
+    #dlN.requestPositions()
+    #dlN.requestAccounts()
 
-    p = HL1.col_values(25)
+    #  Log Updates
+    #
+    HL1 = gc.wks(workbook,logWks)
+    p = HL1.range(HL1.get_addr_int(headerRow,priceCol)+":"+HL1.get_addr_int(1000,priceCol))
+    d = HL1.range(HL1.get_addr_int(headerRow,deltaCol)+":"+HL1.get_addr_int(1000,deltaCol))
     print p
-    for row in range(5,len(p)+1):
-        if p[row-1] is not None:
-            c = Contract()
-            c.m_symbol = HL1.cell(row,2).value
-            c.m_secType = 'OPT'
-            c.m_exchange = 'SMART'
-            c.m_currency = 'USD'
-            c.m_expiry = HL1.cell(row,4).value
-            c.m_strike = HL1.cell(row,5).value
-            c.m_right = HL1.cell(row,6).value
-            print c
-            dlR.requestHistoricalData(c,100+row)
-
+    for cl in p:
+        if cl.value is not '':
+            c = make_contract(HL1.cell(cl.row,symbolCol).value,'OPT','SMART','USD', \
+                HL1.cell(cl.row,expiryCol).value,HL1.cell(cl.row,strikeCol).value,HL1.cell(cl.row,rightCol).value)
+            #dlR.requestHistoricalData(c,100+cl.row)
+            dlR.requestData(c,100+cl.row)
 
     sleep(10)
+
+    print p
+    HL1.update_cells(p)
+    HL1.update_cells(d)
 
     dlR.disconnect()
     dlN.disconnect()
